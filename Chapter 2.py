1b02aad35eec8e2448461c1083a6f5aacba67c6c
Anita Hailey <pengchengma720@gmail.com>
2017-08-05 17:10:02 +0800

    Update Chapter 2.py

--- Chapter 2.py
+++ Chapter 2.py
@@ -8,7 +8,33 @@ print(name.upper())
 print(name.lower())
 
 #2-5
-name = "Immanuel Kant"
-says = "Only two things in the world is worth deeply admire，a bright star is our head，another is our lofty moral law within.\""
-print(name + "once said:\"",says)
+print("Immanuel Kantonce said:\" Only two things in the world is worth deeply admire，a bright star is our head，another is our lofty moral law within.\"")
 
+#2-6
+famous_person = "Immanuel Kant"
+message = "Only two things in the world is worth deeply admire，a bright star is our head，another is our lofty moral law within.\""
+print(famous_person + "once said:\"",message)
+
+#2-7
+name = "  \tAnita\nHailey\n\t"
+
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+#2-8
+print(1+3+3+1)
+print(9-1)
+print(2*4)
+print(16/2)
+
+#2-9
+number = 72
+print("my favorate number is:",str(number))
+
+#2-10
+#code in 2017.7.28 21:12
+
+#2-11
+import this 
