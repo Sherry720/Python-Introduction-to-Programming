0f8ad25388f86499f733cdd24ac86a179511ba3d
Anita Hailey <pengchengma720@gmail.com>
2017-08-05 16:54:58 +0800

    Update Chapter 2.py

--- Chapter 2.py
+++ Chapter 2.py
@@ -1,14 +1,14 @@
-+#2-3
-+name = "Anita Hailey,"
-+print("Hello,",name,"would you like to learn some Python today?")
-+
-+#2-4
-+print(name.title())
-+print(name.upper())
-+print(name.lower())
-+
-+#2-5
-+name = "Immanuel Kant"
-+says = "Only two things in the world is worth deeply admire，a bright star is our head，another is our lofty moral law within.\""
-+print(name + "once said:\"",says)
-+
+#2-3
+name = "Anita Hailey,"
+print("Hello,",name,"would you like to learn some Python today?")
+
+#2-4
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#2-5
+name = "Immanuel Kant"
+says = "Only two things in the world is worth deeply admire，a bright star is our head，another is our lofty moral law within.\""
+print(name + "once said:\"",says)
+
