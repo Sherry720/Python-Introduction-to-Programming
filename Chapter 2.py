ba90cf15c0f76c65493f0cbc7aacdd9c5dbf1654
Anita Hailey <pengchengma720@gmail.com>
2017-08-05 16:51:45 +0800

    Create Chapter 2.py

---
+++ Chapter 2.py
@@ -0,0 +1,14 @@
++#2-3
++name = "Anita Hailey,"
++print("Hello,",name,"would you like to learn some Python today?")
++
++#2-4
++print(name.title())
++print(name.upper())
++print(name.lower())
++
++#2-5
++name = "Immanuel Kant"
++says = "Only two things in the world is worth deeply admire，a bright star is our head，another is our lofty moral law within.\""
++print(name + "once said:\"",says)
++
