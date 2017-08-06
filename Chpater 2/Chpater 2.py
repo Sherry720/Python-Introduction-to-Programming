b63192291c974b3141e3ff9ec293cadef65c8d0c
Anita Hailey <pengchengma720@gmail.com>
2017-08-06 13:30:17 +0800

    变量和简单数据类型

---
+++ Chpater 2/Chpater 2.py
@@ -0,0 +1,40 @@
+#2-3
+name = "Anita Hailey,"
+print("Hello,",name,"would you like to learn some Python today?")
+
+#2-4
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#2-5
+print("Immanuel Kantonce said:\" Only two things in the world is worth deeply admire，a bright star is our head，another is our lofty moral law within.\"")
+
+#2-6
+famous_person = "Immanuel Kant"
+message = "Only two things in the world is worth deeply admire，a bright star is our head，another is our lofty moral law within.\""
+print(famous_person + "once said:\"",message)
+
+#2-7
+name = "  \tAnita\nHailey\n\t"
+
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+#2-8
+print(1+3+3+1)
+print(9-1)
+print(2*4)
+print(16/2)
+
+#2-9
+number = 72
+print("my favorate number is:",str(number))
+
+#2-10
+#code in 2017.7.28 21:12
+
+#2-11
+import this 
