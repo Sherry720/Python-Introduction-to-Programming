bb50aef6c6ed604e6ea4f3374e5c3e593bf552ea
Anita Hailey <pengchengma720@gmail.com>
2017-09-08 23:57:53 -0700

    Introducing Lists

--- Chpater 3/Chapter 3.py
+++ Chpater 3/Chapter 3.py
@@ -30,9 +30,9 @@ print("Hello,"+ name[3] + "," + "I would like to invite you to have a dinner wit
 #3-6
 
 print("I find a bigger table that can contain more people.")
-name.insert(0,"涼宮春日")  #　suzumiya haruhi
+name.insert(0,"涼宮春日")  #　suzumiya haruhi  insert任意位置插入
 name.insert(2,"長門有希")   #　nagato yuki
-name.append("朝比奈みくる") #　asahina mikuru  append不返回值
+name.append("朝比奈みくる") #　asahina mikuru  append添加至列表末尾
 x = name[:]
 print("Hello,"+ name[0] + "," + "I would like to invite you to have a dinner with me")
 print("Hello,"+ name[1] + "," + "I would like to invite you to have a dinner with me")
@@ -42,19 +42,19 @@ print("Hello,"+ name[4] + "," + "I would like to invite you to have a dinner wit
 print("Hello,"+ name[5] + "," + "I would like to invite you to have a dinner with me")
 print("Hello,"+ name[6] + "," + "I would like to invite you to have a dinner with me")
 
-#3-7
+#3-7  #删除元素
 
 print("I'm sorry to tell you that wo can only invite two of you because the table is on the way.")
-print("I'm sorry that you can't come with us for dinner,sorry" + " " + name.pop() + "." )
-print("I'm sorry that you can't come with us for dinner,sorry" + " " + name.pop() + "." )
+print("I'm sorry that you can't come with us for dinner,sorry" + " " + name.pop() + "." ) #pop删除末尾值，并使用这个值
+print("I'm sorry that you can't come with us for dinner,sorry" + " " + name.pop() + "." ) #pop也可删除任意位置，如name.pop(-1)
 print("I'm sorry that you can't come with us for dinner,sorry" + " " + name.pop() + "." )
 print("I'm sorry that you can't come with us for dinner,sorry" + " " + name.pop() + "." )
 print("I'm sorry that you can't come with us for dinner,sorry" + " " + name.pop() + "." )
 print("I'm glad that you can come with us for dinner," +  name[0] + "." )
 print("I'm glad that you can come with us for dinner," +  name[1]+ "." )
 print(name)
-name.remove('涼宮春日')
-name.remove('Mikoto Misaka')
+name.remove('涼宮春日') #del name[0]效果一样
+name.remove('Mikoto Misaka')  #remove是根据值来删除，与位置无关
 print(name)
 
 #3-8
@@ -62,18 +62,18 @@ print(name)
 places = ['los_Angeles','london','firenze','changbai_Mountains','kyoto']  #kyoto　京都
 print(places)
 print(sorted(places))   #顺序排列
-print(places)   #检验，sorted不改变原列表值
+print(places)   #检验，sorted不改变原列表值，sort永久性改变
 print(sorted(places,reverse = True))    #反转排列
 print(places)
 places.reverse()    #反转排列，改变places的值，此时将66-67合并成 print(places.reverse())则返回值为NONE
 print(places)
-places.reverse()    #再反转则变回来了
+places.reverse()    #再反转则变回来了，reverse跟首字母顺序无关，就是反转元素原来排列顺序
 print(places)
 places.sort()
 print(places)   # 同上，合并成print(places.sort())则返回值为none
-places.sort(reverse = True)
+places.sort(reverse = True) #按反序排列
 print(places)   #sort跟sorted用法不同
 
 #3-9
 
-print(len(x))
+print(len(x)) #len()获得列表长度
