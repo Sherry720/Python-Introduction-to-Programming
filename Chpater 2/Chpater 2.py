ea152c3cb4a3c42a24e8df9bf191687fec7781f5
Anita Hailey <pengchengma720@gmail.com>
2017-09-08 19:39:11 -0700

    Update Chpater 2.py

--- Chpater 2/Chpater 2.py
+++ Chpater 2/Chpater 2.py
@@ -2,7 +2,7 @@
 name = "Anita Hailey,"
 print("Hello,",name,"would you like to learn some Python today?")
 
-#2-4
+#2-4  #大小写处理
 print(name.title())
 print(name.upper())
 print(name.lower())
@@ -15,13 +15,13 @@ famous_person = "Immanuel Kant"
 message = "Only two things in the world is worth deeply admire，a bright star is our head，another is our lofty moral law within.\""
 print(famous_person + "once said:\"",message)
 
-#2-7
+#2-7  #删除空白
 name = "  \tAnita\nHailey\n\t"
 
-print(name)
-print(name.lstrip())
-print(name.rstrip())
-print(name.strip())
+print(name) #删除是暂时的
+print(name.lstrip())  #删除开头空白
+print(name.rstrip())  #删除结尾空白
+print(name.strip()) #同时删除前后空白
 
 #2-8
 print(1+3+3+1)
